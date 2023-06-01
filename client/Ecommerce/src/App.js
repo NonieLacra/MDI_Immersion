ff51b94fe129ed9420d6d409821ebe49fc72109d
Nifferg <kyle.baguio@novare.com.hk>
2023-06-01 19:27:47 +0800

    designed loginForm

--- client/Ecommerce/src/App.js
+++ client/Ecommerce/src/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
+import LoginForm from './components/loginForm';
 
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       
+      <LoginForm />
     </div>
   );
 }
---
+++ client/Ecommerce/src/components/loginform.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../style/loginForm.css";
+
+
+const LoginForm = () => {
+    return(
+        <div className="container">
+            <h1>Login</h1>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+
+            <div className="login-btn"> Login</div>
+            <div className="register-btn">Register</div>
+            <p className="text">Login using </p>
+            <div className="alt-login">
+                <div className="google"></div>
+            </div>
+        </div>
+
+        
+    )
+}
+
+export default LoginForm
