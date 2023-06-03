384ab086a6aafd7c47c418f43e4a4a2710cead72
nonie.lacra <101985410+NonieLacra@users.noreply.github.com>
2023-06-03 12:15:04 +0800

    Update App.js

--- client/Ecommerce/src/App.js
+++ client/Ecommerce/src/App.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import LoginForm from './components/loginform.js';
 import RegistrationForm from './components/Registration/RegistrationForm.js';
+import LoginForm from './components/Login/loginform.js';
 import Header from './components/HomePage/Header.js';
 import Body from './components/HomePage/Body.js';
-import '../src/styles/loginform.css'
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path='/' element={
-            <div>
-          <Header />
-          <Body />
-            </div>
-          } />
-          <Route path='/login' element={<LoginForm />} />        
-          <Route path='/register' element={<RegistrationForm />} />    
-        </Routes>
-      </Router>
-      <div></div>
+     
+     
+    
+   
+      
     </div>
   );
 }
